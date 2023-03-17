95631f491c4383da1a7c8dc69f546591765fc68d
kihno <stistyle13@gmail.com>
2023-03-17 12:35:45 -0700

    Create tweet model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-has_person_name
+  has_person_name
+
+  has_many :tweets
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
