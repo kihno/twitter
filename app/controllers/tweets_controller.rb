abc5b043025c458f3ceaa3466eb57ef29862496a
kihno <stistyle13@gmail.com>
2023-03-17 17:23:11 -0700

    Authenticated users can post, others can view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @tweet = Tweet.new
