55a970e6ec96c27a88218859abf042a1ad9d0f2e
kihno <stistyle13@gmail.com>
2023-03-20 15:08:09 -0700

    Add retweeting functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_17_223823) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_200058) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_17_223823) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "tweet_id"
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
